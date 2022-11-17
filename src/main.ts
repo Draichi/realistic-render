f458f33c82bc7315d0f268e82b6bc94f9f959853
@Draichi <lucasdraichi@gmail.com>
2022-11-16 20:27:14 -0500

    feat: add environment maps

--- src/main.ts
+++ src/main.ts
@@ -3,12 +3,36 @@ import * as THREE from 'three'
 import GUI from 'lil-gui'
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls'
 import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader'
+import nx from '/environmentMaps/6/nx.png'
+import ny from '/environmentMaps/6/ny.png'
+import nz from '/environmentMaps/6/nz.png'
+import px from '/environmentMaps/6/px.png'
+import py from '/environmentMaps/6/py.png'
+import pz from '/environmentMaps/6/pz.png'
 
 const canvas = document.querySelector('canvas#webgl') as HTMLCanvasElement
 
 const gui = new GUI()
 
 const scene = new THREE.Scene()
+
+const updateAllMeterials = () => {
+  scene.traverse((child) => {
+    const isMesh = child instanceof THREE.Mesh
+    if (!isMesh) {
+      return
+    }
+
+    const isStandardMaterial =
+      child.material instanceof THREE.MeshStandardMaterial
+    if (!isStandardMaterial) {
+      return
+    }
+
+    child.material.envMap = environmentMaps
+    child.material.envMapIntensity = 2.5
+  })
+}
 
 const gltfLoader = new GLTFLoader()
 gltfLoader.load(
@@ -18,6 +42,8 @@ gltfLoader.load(
     modelScene.scale.set(4, 4, 4)
     modelScene.rotateY(Math.PI * 1.75)
     scene.add(modelScene)
+    updateAllMeterials()
+
     gui
       .add(modelScene.rotation, 'y')
       .min(-Math.PI)
@@ -26,6 +52,10 @@ gltfLoader.load(
       .name('Model Rotation')
   }
 )
+
+const cubeTextureLoader = new THREE.CubeTextureLoader()
+const environmentMaps = cubeTextureLoader.load([px, nx, py, ny, pz, nz])
+scene.background = environmentMaps
 
 const sphere01 = new THREE.Mesh(
   new THREE.SphereGeometry(1, 32, 32),
