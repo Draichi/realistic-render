83ca2603620887989c4cbd1efb44b16a2709a044
@Draichi <lucasdraichi@gmail.com>
2022-11-19 23:42:36 -0500

    feat: tweak model properties

--- src/main.ts
+++ src/main.ts
@@ -3,12 +3,12 @@ import * as THREE from 'three'
 import GUI from 'lil-gui'
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls'
 import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader'
-import nx from '/environmentMaps/6/nx.png'
-import ny from '/environmentMaps/6/ny.png'
-import nz from '/environmentMaps/6/nz.png'
-import px from '/environmentMaps/6/px.png'
-import py from '/environmentMaps/6/py.png'
-import pz from '/environmentMaps/6/pz.png'
+import nx from '/environmentMaps/3/nx.jpg'
+import ny from '/environmentMaps/3/ny.jpg'
+import nz from '/environmentMaps/3/nz.jpg'
+import px from '/environmentMaps/3/px.jpg'
+import py from '/environmentMaps/3/py.jpg'
+import pz from '/environmentMaps/3/pz.jpg'
 
 const canvas = document.querySelector('canvas#webgl') as HTMLCanvasElement
 
@@ -33,9 +33,11 @@ const updateAllMeterials = () => {
     }
 
     child.material.envMapIntensity = debug.envMapIntensity
+
+    child.castShadow = true
+    child.receiveShadow = true
   })
 }
-
 const gltfLoader = new GLTFLoader()
 gltfLoader.load(
   '/FlightHelmet/glTF/FlightHelmet.gltf',
@@ -54,6 +56,13 @@ gltfLoader.load(
       .name('Model Rotation')
   }
 )
+gltfLoader.load('/burger.glb', ({ scene: modelScene }) => {
+  modelScene.position.set(2, -1.5, 2)
+  modelScene.scale.set(0.2, 0.2, 0.2)
+  modelScene.visible = false
+  scene.add(modelScene)
+  gui.add(modelScene, 'visible').name('Show hamburger')
+})
 
 const cubeTextureLoader = new THREE.CubeTextureLoader()
 const environmentMaps = cubeTextureLoader.load([px, nx, py, ny, pz, nz])
@@ -69,6 +78,10 @@ gui
 
 const directionalLight = new THREE.DirectionalLight('#ffffff', 4)
 directionalLight.position.set(0.25, 3, 2.25)
+directionalLight.castShadow = true
+directionalLight.shadow.camera.far = 15
+directionalLight.shadow.mapSize.set(1024, 1024)
+directionalLight.shadow.normalBias = 0.05
 scene.add(directionalLight)
 gui
   .add(directionalLight, 'intensity')
@@ -95,6 +108,15 @@ gui
   .step(0.001)
   .name('Light Z position')
 
+const directionalLightCameraHelper = new THREE.CameraHelper(
+  directionalLight.shadow.camera
+)
+directionalLightCameraHelper.visible = false
+scene.add(directionalLightCameraHelper)
+gui
+  .add(directionalLightCameraHelper, 'visible')
+  .name('Directional Camera Helper')
+
 const camera = new THREE.PerspectiveCamera(
   75,
   window.innerWidth / window.innerHeight,
@@ -107,10 +129,31 @@ const controls = new OrbitControls(camera, canvas)
 controls.enableDamping = true
 controls.dampingFactor = 0.02
 
-const renderer = new THREE.WebGLRenderer({ canvas })
+const isAntialiasActivated: boolean = window.devicePixelRatio < 2
+const renderer = new THREE.WebGLRenderer({
+  canvas,
+  antialias: isAntialiasActivated,
+})
 renderer.setSize(window.innerWidth, window.innerHeight)
 renderer.physicallyCorrectLights = true
 renderer.outputEncoding = THREE.sRGBEncoding
+renderer.shadowMap.enabled = true
+renderer.shadowMap.type = THREE.PCFSoftShadowMap
+gui
+  .add(renderer, 'toneMapping', {
+    None: THREE.NoToneMapping,
+    Linear: THREE.LinearToneMapping,
+    Reinhard: THREE.ReinhardToneMapping,
+    Cineon: THREE.CineonToneMapping,
+    ACESFilmic: THREE.ACESFilmicToneMapping,
+  })
+  .name('Tone Mapping Algorithm')
+gui
+  .add(renderer, 'toneMappingExposure')
+  .min(0)
+  .max(10)
+  .step(0.001)
+  .name('Tone Mapping Exposure')
 
 const tick = () => {
   controls.update()
